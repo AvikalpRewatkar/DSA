81880e0f1d079d79366a7cf5eefc894a510ed3ed
AvikalpRewatkar <rewavikalp@gmail.com>
2024-06-16 16:55:36 +0530

    Moved all Data structures to DS folder

---
+++ JAVA/DS/Tree/BSTImplementation.java
@@ -0,0 +1,17 @@
+package Tree;
+
+public class BSTImplementation {
+    class Node {
+        int data;
+        Node leftNode;
+        Node rightNode;
+    }
+    void createBST() {
+        
+    }
+    public static void main(String[] args) {
+        BSTImplementation bst = new BSTImplementation();
+        bst.createBST();
+
+    }    
+}
